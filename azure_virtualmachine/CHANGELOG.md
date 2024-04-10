529c66bab434b27e2fb4790c8f708003fc6e5e4c
rohit <rohit.yadav@bluepi.in>
2024-04-10 12:02:10 +0530

    tf

--- azure_virtualmachine/CHANGELOG.md
+++ azure_virtualmachine/CHANGELOG.md
@@ -11,3 +11,5 @@ Creating new Module for Azure Virtual Machine
 ## 1.0.33 - 2024-04-08
 
 ## 1.0.34 - 2024-04-08
+
+## 1.0.35 - 2024-04-08
--- azure_virtualmachine/packer_image.tf
+++ azure_virtualmachine/packer_image.tf
@@ -1,30 +1,34 @@
-provider "azurerm" {
-  alias           = "shared-image"
-  subscription_id = "f2fb6ada-1520-4d1a-a23b-8ebbcf63a24c"
-  tenant_id       = "23e4ff6d-7f45-4d40-8565-6135271fcd0c"
-  features {}
-}
+#provider "azurerm" {
+ #subscription_id = "f2fb6ada-1520-4d1a-a23b-8ebbcf63a24c"
+ # tenant_id       = "23e4ff6d-7f45-4d40-8565-6135271fcd0c"
+ # features {}
+#}
 
-data "azurerm_shared_image_version" "image_2016" {
-  name                = "latest"
-  image_name          = "Win2016"
-  gallery_name        = "CIImagegallery"
-  resource_group_name = "RG-ADMIN-OS-Repository"
-  provider            = azurerm.shared-image
-}
+#data "azurerm_shared_image_version" "image_2016" {
+ # name                = "latest"
+ # image_name          = "Win2016"
+ # gallery_name        = "CIImagegallery"
+ # resource_group_name = "RG-ADMIN-OS-Repository"
+ # provider            = azurerm.shared-image
+#}
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-datacenter"
+    version   = "latest"
+  }
+#data "azurerm_shared_image_version" "image_2019" {
+ # name                = "latest"
+  #image_name          = "Win2019"
+  #gallery_name        = "CIImagegallery"
+  #resource_group_name = "RG-ADMIN-OS-Repository"
+  #provider            = azurerm.shared-image
+#}
 
-data "azurerm_shared_image_version" "image_2019" {
-  name                = "latest"
-  image_name          = "Win2019"
-  gallery_name        = "CIImagegallery"
-  resource_group_name = "RG-ADMIN-OS-Repository"
-  provider            = azurerm.shared-image
-}
-
-data "azurerm_shared_image_version" "image_2022" {
-  name                = "latest"
-  image_name          = "Win2022"
-  gallery_name        = "CIImagegallery"
-  resource_group_name = "RG-ADMIN-OS-Repository"
-  provider            = azurerm.shared-image
-}
+#data "azurerm_shared_image_version" "image_2022" {
+ # name                = "latest"
+  #image_name          = "Win2022"
+  #gallery_name        = "CIImagegallery"
+  #resource_group_name = "RG-ADMIN-OS-Repository"
+  #provider            = azurerm.shared-image
+#}
